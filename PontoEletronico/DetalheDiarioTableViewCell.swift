5ffc3defc3a0f90a2e1507cf8492bde02ae062b7
Amanda <amanda96campos@gmail.com>
2015-06-03 12:23:48 -0300

    StoryBoard e classes

---
+++ PontoEletronico/DetalheDiarioTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetalheDiarioTableViewCell.swift
+//  PontoEletronico
+//
+//  Created by Amanda Guimaraes Campos on 03/06/15.
+//  Copyright (c) 2015 Amanda Guimaraes Campos. All rights reserved.
+//
+
+import UIKit
+
+class DetalheDiarioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PontoEletronico/PontoEletronico/DiarioTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DiarioTableViewCell.swift
+//  PontoEletronico
+//
+//  Created by Amanda Guimaraes Campos on 03/06/15.
+//  Copyright (c) 2015 Amanda Guimaraes Campos. All rights reserved.
+//
+
+import UIKit
+
+class DiarioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
