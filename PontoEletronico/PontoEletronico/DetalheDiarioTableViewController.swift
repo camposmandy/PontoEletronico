fc399f0bdb9dc297474b27c1db18647e60a70d5e
LeonardoBrunassi <leo.brunassi.apple@hotmail.com>
2015-06-11 14:37:41 -0300

    Página de detalhes quase pronta. Falta apenas o crédito e débito.

--- PontoEletronico/PontoEletronico/DetalheDiarioTableViewController.swift
+++ PontoEletronico/PontoEletronico/DetalheDiarioTableViewController.swift
@@ -20,18 +20,37 @@ class DetalheDiarioTableViewController: UITableViewController {
     
     var diaTrab : DiaTrabalhado!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         preencherLabels()
-
     }
     
+    
     func preencherLabels(){
-        diaTrabalhado.text = "\(diaTrab.horaEntrada)"
-        horaEntrada.text = "\(diaTrab.horaEntrada)"
-        horaSaida.text = "\(diaTrab.horaEntrada)"
-        horaSaidaAlmoco.text = "\(diaTrab.horaEntrada)"
-        horaVoltaAlmoco.text = "\(diaTrab.horaEntrada)"
+        
+        //////////// Formatação do dia Trabalhado ////////////
+        var dayFormatter = NSDateFormatter()
+        dayFormatter.dateFormat = "dd/MM/yyyy"
+        
+        var hourFormatter = NSDateFormatter()
+        hourFormatter.dateFormat = "HH:mm:ss"
+        
+        var diaTrabalhadoString = dayFormatter.stringFromDate(diaTrab.horaEntrada)
+        
+        var horaEntradaString = hourFormatter.stringFromDate(diaTrab.horaEntrada)
+        
+        var horaSaidaString = hourFormatter.stringFromDate(diaTrab.horaSaida)
+        
+        var horaSaidaAlmocoString = hourFormatter.stringFromDate(diaTrab.horaSaidaAlmoco)
+        
+        var horaVoltaAlmocoString = hourFormatter.stringFromDate(diaTrab.horaVoltaAlmoco)
+        
+        diaTrabalhado.text = diaTrabalhadoString
+        horaEntrada.text = horaEntradaString
+        horaSaida.text = horaSaidaString
+        horaSaidaAlmoco.text = horaSaidaAlmocoString
+        horaVoltaAlmoco.text = horaVoltaAlmocoString
         creditoLbl.text = "\(diaTrab.horaEntrada)"
     }
     
